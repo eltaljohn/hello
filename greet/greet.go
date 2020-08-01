e27525a859304d7fc35b4e8311a6dbcb993b70eb
johneduard <jhonedwardbol@gmail.com>
2020-08-01 09:04:36 -0500

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "🙋‍♂️"
+
+// English retorna saludo en inglés
+func English() string {
+	return "Hi " + emoji
+}
+
+// Italian returna saludo en italiano
+func Italian() string {
+	return "Ciao " + emoji
+}
